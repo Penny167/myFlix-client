83ad6b2571a0377009c54f62c2b02c7effedb5ed
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-06-14 18:57:11 +0100

    Created MainView component.

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+class MainView extends React.Component {
+  render() {
+    return (
+      <div className="main-view">
+        <div>The Godfather</div>
+        <div>Dr Zhivago</div>
+        <div>Lawrence of Arabia</div>
+      </div>
+    )
+  }
+}
+
+export default MainView;
