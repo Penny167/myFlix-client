d791ef920e42195311754e2be642b45a442bd906
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-06-16 12:11:23 +0100

    Added a movies state to the MainView component.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import MovieCard from '../movie-card/movie-card';
 
 class MainView extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      movies: [
+        {_id: 1, Title: 'The Godfather', Description: 'descript 1', ImageUrl: '...'},
+        {_id: 2, Title: 'Dr Zhivago', Description: 'descript 2', ImageUrl: '...'},
+        {_id: 3, Title: 'Lawrence of Arabia', Description: 'descript 3', ImageUrl: '...'}
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="main-view">
