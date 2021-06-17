4e29080a0dae9b62ec9c1be70a39bfbb147f41b7
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-06-17 23:31:21 +0100

    Added a back button for returning to the MainView.

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -14,6 +14,7 @@ class MovieView extends React.Component {
         <span className="Label">Description: </span>
         <span className="Value">{movieData.Description}</span>
       </div>
+      <button>Back</button>
     </div>
     );
   }
