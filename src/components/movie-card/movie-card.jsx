6a6f4ffa0dd9c1b57a356dc226f7c718d6f2a647
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-06-17 16:37:59 +0100

    Updated props to include onMovieClick function and added event listener attribute invoking function to movie-card div element.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-    const { movieData } = this.props;
-    return <div className="movie-card">{movieData.Title}</div>;
+    const { movieData, onMovieClick } = this.props;
+    return <div className="movie-card" onClick={() => {onMovieClick(movieData)}}>
+      {movieData.Title}</div>
   }
 }
 
