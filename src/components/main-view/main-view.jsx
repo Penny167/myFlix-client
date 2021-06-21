550bf7ff754c2f0d83109f46f7d83528cfa112a7
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-06-21 19:54:36 +0100

    Removed "The list is empty" message.
    This could cause confusion while the data is loading.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -29,7 +29,7 @@ class MainView extends React.Component {
 
   render() {
     const { movies, selectedMovie } = this.state;
-    if (movies.length === 0) return <div className="main-view">The list is empty</div>;
+    if (movies.length === 0) return <div className="main-view" />;
     return (
       <div className="main-view">
         {selectedMovie
