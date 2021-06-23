fe971a21cbee0993e92de44cf82d39165753622b
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-06-23 20:17:29 +0100

    Created the LoginView component.

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,35 @@
+// Import useState hook from the React library
+import {useState} from 'react';
+
+// Create function component
+function LoginView(props) {
+
+// Use destructuring to set initial values for username and password and name functions to update values
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+/* Function to handle submission of form data. Note that this will be updated to send a request
+for proper authentication of credentials in a future exercise */
+  const handleSubmit = () => {
+    e.preventDefault();
+    console.log(username, password);
+/* We will give the component a property called onLoggedIn within MainView. We will then call it here
+passing the username as the parameter */
+//  props.onLoggedIn(username);
+  }
+
+// Return statement
+  return(
+    <form>
+      <label>Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>Password:
+        <input type="text" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+    </form>
+  );
+}
+
+export default LoginView
