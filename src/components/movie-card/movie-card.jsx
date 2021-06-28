4046191de41334f1e23a5dad921ce49350937f9d
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-06-28 12:50:23 +0100

    Imported Button and Card components from react-bootstrap in to movie-card component.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap';
+import Card from 'react-bootstrap';
 
 class MovieCard extends React.Component {
   render() {
