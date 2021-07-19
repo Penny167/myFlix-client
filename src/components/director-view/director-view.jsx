dbf080841f6039b1aa3c299af4ea7695ce9eb222
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-07-19 17:31:04 +0100

    Created basic director and genre views.

---
+++ src/components/director-view/director-view.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function DirectorView() {
+return(
+  <div className="director-view">
+    <div>Director name</div>
+    <div>Bio</div>
+    <div>Birth year</div>
+    <div>Death year</div>
+  </div>
+);
+}
+
+export default DirectorView
---
+++ src/components/genre-view/genre-view.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function GenreView() {
+return(
+  <div className="director-view">
+    <div>Director name</div>
+    <div>Bio</div>
+    <div>Birth year</div>
+    <div>Death year</div>
+  </div>
+);
+}
+
+export default GenreView
