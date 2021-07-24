8976b2a7eb64152ed452744e3b3114489bdf5ee0
Penny167 <78602066+Penny167@users.noreply.github.com>
2021-07-24 12:37:38 +0100

    Created basic profile view component and css file.

---
+++ src/components/profile-view/profile-view.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function ProfileView() {
+
+
+return (
+  <div className="profile">
+    <h2>myProfile</h2>
+    <p>Username:</p>
+    <p>Username:</p>
+    <p>Username:</p>
+    <p>Username:</p>
+    <br />
+    <h4>myMovies</h4>
+    <div>movie data</div>
+  </div>
+)
+
+}
+
+export default ProfileView
